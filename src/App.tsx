57b6b5e37c0db6fc8f30ad7d446448178a0dfa04
Hugo Skyrons <hugosouzaoficial4@gmail.com>
2023-09-21 23:04:08 -0300

    file changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,55 @@
+import { FormEvent } from "react";
+
+import style from "./global.module.css"
+import rocket from "./assets/rocket.png"
+import todo from "./assets/todo.png"
 
 function App() {
 
-  return (
-    <div>
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+    console.log("Creating new task");
+  }
 
-      <header className="header">
-        <img src="" className="rocket-logo" alt="rocket" />
-        <img src="" className="todo-logo" alt="todo"/>
+
+  const task =[
+    'texto 1',
+    'texto 2',
+  ]
+
+  return (
+    <div className={style.page}>
+
+      <header className={style.header}>
+        <img src={rocket} className="rocket-logo" alt="rocket" />
+        <img src={todo} className="todo-logo" alt="todo"/>
       </header>
 
-      <div className="wrapper">    
-        <form action="">
+      <div className={style.wrapper}>    
+        <form onSubmit={handleCreateNewTask}>
           <input type="text" />
-          <button className="{style.create-task-button}">Criar</button>
+          <button
+            type="submit"
+            className="{style.create-task-button}">
+                Criar
+            </button>
         </form>
 
-        <main>
-          <header>
+        <main className={style.content}>
+          <header >
             <div>
-              <span>Tarefas criadas</span>
-              <div>0</div>
+                <span>Tarefas Criadas </span>
+                <span>0</span>
             </div>
-
+              
             <div>
-                <span>Concluidas</span>
-                <div>0</div>
+                <span>Concluidas </span>
+                <span>0</span>
             </div>
-            
           </header>
 
           <div>
-            lista de tarefas
+            {task.map(task => task)}
           </div>
 
         </main>
